40362f38ef0bd31368afb819e15c2489cf07f212
Emre Cetinkaya <emrecetinkaya@Emres-MBP.attlocal.net>
2022-10-20 00:00:38 -0700

    Unnecessary update button deleted

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -5,12 +5,6 @@ import Card from "../UI/Card"
 
 const ExpenseItem = (props) => {
 
-    const [title, setTitle] = useState(props.title);
-
-    const clickHandler = () => {
-        setTitle('Updated');
-    }
-
     return (
         <li>
             <Card className="expense-item">
@@ -18,7 +12,6 @@ const ExpenseItem = (props) => {
                 <div className="expense-item__description">
                     <h2>{props.title}</h2>
                     <div className="expense-item__price">${props.amount}</div>
-                    <button onClick={clickHandler}>Change Title</button>
                 </div>
             </Card>
         </li>
